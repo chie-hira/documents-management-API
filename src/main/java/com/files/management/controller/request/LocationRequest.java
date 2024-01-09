0111aa57d334146ff8c51d614284dcb530154781
chie-hira <capella.a36@gmail.com>
2024-01-09 17:13:05 +0900

    400バリデーション実装

--- src/main/java/com/files/management/controller/request/LocationRequest.java
+++ src/main/java/com/files/management/controller/request/LocationRequest.java
@@ -1,9 +1,12 @@
 package com.files.management.controller.request;
 
-import com.files.management.controller.response.LocationResponse;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Positive;
 
 public class LocationRequest {
+    @NotBlank(message ="location is required")
     private String location;
+    @Positive(message = "shelfNumber is required")
     private int shelfNumber;
     public LocationRequest(String location, int shelfNumber) {
         this.location = location;
